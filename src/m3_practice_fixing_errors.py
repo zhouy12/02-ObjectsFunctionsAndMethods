2779d857a840f727c8de5cee2477129c6818565b
zhouy12 <zhouy12@rose-hulman.edu>
2018-09-03 17:04:25 -0400

    done with m3

--- src/m3_practice_fixing_errors.py
+++ src/m3_practice_fixing_errors.py
@@ -7,7 +7,7 @@ Authors: David Mutchler, Dave Fisher, Vibha Alangar, Amanda Stouder,
 
 ###############################################################################
 #
-# TODO: 2.
+# DONE: 2.
 #   Locate the syntax (notation) errors in this file
 #   by looking for red underlines.
 #
